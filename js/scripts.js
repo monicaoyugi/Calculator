16b65978f3a5c1885a863cdacb85dd23308a5202
monicaoyugi <monicaoyugi@gmail.com>
2019-11-28 14:30:16 +0300

    second calculations

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+// var add = function(number1, number2) {
+//     return number1 + number2;
+//   };
+  
+//   var number1 = parseInt(prompt("Enter a number:"));
+//   var number2 = parseInt(prompt("Enter another number:"));
+//   var result = add(number1, number2);
+//   alert(result);
+
+
+// var bmi = function(weight, height){
+//     return weight/(height*height)
+// }
+// var weight = prompt("Enter your weight in m")
+// weight = parseInt(weight);
+// var height = prompt("Enter your height in m")
+// height = parseInt(height);
+// alert(bmi(weight,height))
+
+// var num = true
+// if num {
+//     console.log("print even number")
+// }
+// else  console.log("print odd number")
+
+
+// var num = 3;
+// if(num%2==0){
+//     console.log("Even no");
+// }
+// else{
+//     console.log("Odd no");
+// }
+
+
+var temperature  = parseInt(prompt("enter number"));
+
+if (temperature < 0) {
+    console.log("Brr! Don't freeze out there!");
+  } else if (temperature < 15) {
+    alert("Don't forget a jacket!");
+  } else if (temperature < 25) {
+   alert("Have a nice day");
+  } else {
+     alert("Keep cool today!");
+     }
+  
+
+// temperature = prompt("Enter tehe temperature");
+// temperature = parseInt(temperature);
+  alert(displayTemp(temp))
